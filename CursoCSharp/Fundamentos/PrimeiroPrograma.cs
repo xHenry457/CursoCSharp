d492c5da56c18ed019d8721f6a52dd17f22ce170
Henrique <Henrique@DESKTOP-F5EQQNT>
2021-08-19 12:08:29 -0300

    Adicionar arquivos de projeto.

---
+++ CursoCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CursoCSharp.Fundamentos {
+    class PrimeiroPrograma {
+        static void Main() {
+            Console.Write("Primeiro ");
+            Console.WriteLine("Programa");
+            Console.WriteLine("Terminou!");
+        }
+    }
+}
---
+++ CursoCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CursoCSharp {
+    class Program {
+        static void Main2(string[] args) {
+            Console.WriteLine("Hello World!");
+
+            {
+                Console.WriteLine("Hello World 2!");
+            }
+        }
+    }
+}
