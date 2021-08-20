00f440a202324c6c3d6180d076cac08578ab6e2e
Henrique Gonçalves <59718253+xHenry457@users.noreply.github.com>
2021-08-20 12:43:03 -0300

    Add files via upload

---
+++ CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos    
+ {
+ /// <summary>
+ /// 
+ /// </summary>
+    class Comentarios 
+     {
+        public static void Executar() { 
+            // Cuidado com os comentários desnecessarios
+            Console.WriteLine("Codigo claro é sempre melhor!");  
+
+            /*
+             * Esse é um comentatário
+             * de multiplas linhas. . .
+             */
+            Console.WriteLine("O C# tem o XML Comments");           
+        }
+    }
+}
