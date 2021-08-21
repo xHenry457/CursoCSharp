14daa7fb9d5c0eeca4442e217bc377f99fb95936
Henrique Gonçalves <59718253+xHenry457@users.noreply.github.com>
2021-08-21 10:51:26 -0300

    Update Program.cs

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -10,6 +10,7 @@ namespace CursoCSharp {
                 {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
                 {"Comentarios - Fundamentos" , Comentarios.Executar },
                 {"Variaveis e Constantes - Fundamentos", VariaveisEConstantes.Executar },
+                {"Inferencia - Fundamentos", Inferencia.Executar },
 
             }) ;
 
