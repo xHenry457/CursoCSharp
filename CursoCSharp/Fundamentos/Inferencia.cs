8ab44f5c650b3b189e0e4da5757bc8335e7f529d
Henrique Gonçalves <59718253+xHenry457@users.noreply.github.com>
2021-08-21 10:53:27 -0300

    Add files via upload

---
+++ CursoCSharp/Fundamentos/Inferencia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos 
+ {
+    class Inferencia 
+     {
+        public static void Executar () {
+            var nome = "Leonardo";
+            // nome = 123;
+            Console.WriteLine(nome);
+        }
+    }
+}
