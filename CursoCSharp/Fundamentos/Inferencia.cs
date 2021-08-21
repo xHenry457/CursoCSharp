51a1f818c087785776a12a25fe811e20c02f7a98
Henrique Gonçalves <59718253+xHenry457@users.noreply.github.com>
2021-08-21 10:57:28 -0300

    Update Inferencia.cs

--- CursoCSharp/Fundamentos/Inferencia.cs
+++ CursoCSharp/Fundamentos/Inferencia.cs
@@ -10,6 +10,12 @@ namespace CursoCSharp.Fundamentos
             var nome = "Leonardo";
             // nome = 123;
             Console.WriteLine(nome);
+         
+            // int idade;
+            var idade = 32;
+            Console.WriteLine(idade);
+         
+            
         }
     }
 }
