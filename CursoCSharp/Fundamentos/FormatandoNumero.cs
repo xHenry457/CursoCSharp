a2a4091cc05c18269cfed724c438f394cd2fe54c
Henrique Gonçalves <59718253+xHenry457@users.noreply.github.com>
2021-09-02 10:36:56 -0300

    Add files via upload

---
+++ CursoCSharp/Fundamentos/FormatandoNumero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+    class FormatandoNumero 
+     {
+        public static void Executar(){
+            double valor = 15.175;
+
+            //Formata o valor numerico
+            Console.WriteLine(valor.ToString("F1"));
+
+            //coloca em valor monetario
+            Console.WriteLine(valor.ToString("C"));
+
+            //Valor em Percentual
+            Console.WriteLine(valor.ToString("P"));
+
+            //Coloca o valor com casas decimais
+            Console.WriteLine(valor.ToString("#.##"));
+
+        }
+    }
+}
