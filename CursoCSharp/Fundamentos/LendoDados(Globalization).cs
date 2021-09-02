4bbe119e4187b6509cc7bed378f93b8398e3aa7f
Henrique Gonçalves <59718253+xHenry457@users.noreply.github.com>
2021-09-02 10:51:16 -0300

    Add files via upload

---
+++ CursoCSharp/Fundamentos/LendoDados(Globalization).cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization; // Modo Global
+
+namespace CursoCSharp.Fundamentos {
+    class LendoDados {
+        public static void Executar() {
+
+            Console.WriteLine("Qual é o seu nome? ");
+            string nome = Console.ReadLine();
+            //Console.ReadLine = Para ler
+
+            Console.WriteLine("Qual é a sua idade?");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual é o seu salário?");
+            double salario = double.Parse(Console.ReadLine(),
+                CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine($"{nome} {idade} R${salario}");
+            //Interpolação = ($)
+
+
+        }
+    }
+}
