1d0af97672596ee042506e533964e754f3176bde
Henrique Gonçalves <59718253+xHenry457@users.noreply.github.com>
2021-10-18 17:55:05 -0300

    Add files via upload

---
+++ CursoCSharp/Fundamentos/OperadoresLogicos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+    class OperadoresLogicos {
+
+        public static void Executar() {
+
+            var executouTrabalho1 = true;
+            var executouTrabalho2 = false;
+
+            var comprouTv50 = executouTrabalho1 && executouTrabalho2;
+            Console.WriteLine("Comprou a Tv {0}", comprouTv50);
+
+            var comprouSorvete = executouTrabalho1 || executouTrabalho2;
+            Console.WriteLine("Comprou o Sorvete?`{0}", comprouSorvete);
+
+            var comprouTv32 = executouTrabalho1 ^ executouTrabalho2;
+            Console.WriteLine("Comprou a Tv 32? {0}", comprouTv32);
+
+            Console.WriteLine("Mais saudável? {0}", !comprouSorvete);
+        }
+    }
+}
